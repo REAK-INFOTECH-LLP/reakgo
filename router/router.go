868da8ecc7238a6dbacf0db3ebe4bfd6d1740671
Ashish Saxena <ashish@reak.in>
2023-09-19 19:43:19 +0530

    wip

--- router/router.go
+++ router/router.go
@@ -4,8 +4,6 @@ import (
 	"net/http"
 	"reakgo/controllers"
 	"reakgo/utility"
-
-	//"reakgo/utility"
 	"strings"
 )
 
@@ -15,25 +13,25 @@ func Routes(w http.ResponseWriter, r *http.Request) {
 	route := strings.Trim(r.URL.Path, "/")
 	switch route {
 	case "", "index":
-        utility.CheckACL(w, r, 0)
+		utility.CheckACL(w, r, 0)
 		controllers.BaseIndex(w, r)
 	case "login":
-        utility.CheckACL(w, r, 0)
+		utility.CheckACL(w, r, 0)
 		controllers.Login(w, r)
 	case "dashboard":
-        utility.CheckACL(w, r, 1)
+		utility.CheckACL(w, r, 1)
 		controllers.Dashboard(w, r)
 	case "addSimpleForm":
-        utility.CheckACL(w, r, 0)
+		utility.CheckACL(w, r, 0)
 		controllers.AddForm(w, r)
 	case "viewSimpleForm":
-        utility.CheckACL(w, r, 0)
+		utility.CheckACL(w, r, 0)
 		controllers.ViewForm(w, r)
 	case "register-2fa":
-        utility.CheckACL(w, r, 1)
+		utility.CheckACL(w, r, 1)
 		controllers.RegisterTwoFa(w, r)
 	case "verify-2fa":
-        utility.CheckACL(w, r, 1)
+		utility.CheckACL(w, r, 1)
 		controllers.VerifyTwoFa(w, r)
 	}
 }
